5a16131c26049042516bd6d5acfce5964ac957f5
Luis Dias <luis.edudias@gmail.com>
2012-02-26 23:18:31 +0000

    first commit

---
+++ code/SuperSizedPage.php
@@ -0,0 +1,139 @@
+<?php
+/*
+Copyright (c) 2012 Luis E. S. Dias - www.smartbyte.com.br
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+class SuperSizedPage extends Page {
+    
+    static $allowed_children = array(
+        'SuperSizedImage'
+    );
+
+    static $db = array(
+        'AutoPlay' => Boolean,
+        'FitAlways' => Boolean,
+        'FitLandscape' => Boolean,
+        'FitPortrait' => Boolean,
+        'HorizontalCenter' => Boolean,
+        'ImageProtect' => Boolean,
+        'KeyboardNav' => Boolean,
+        'MinHeight' => Int,
+        'MinWidth' => Int,
+        'NewWindow' => Boolean,
+        'PauseHover' => Boolean,
+        'Performance' => Int,
+        'Random' => Boolean,
+        'Slideshow' => Boolean,
+        'SlideInterval' => Int,
+        'SlideLinks' => Varchar,
+        'StartSlide' => Int,
+        'StopLoop' => Boolean,
+        'ThumbLinks' => Boolean,
+        'ThumbnailNavigation' => Boolean,
+        'Transition' => Varchar,
+        'TransitionSpeed' => Int,
+        'VerticalCenter' => Boolean
+    );
+
+    public function populateDefaults() {
+        parent::populateDefaults();
+        $this->Autoplay = 1;
+        $this->FitAlways = 0;
+        $this->FitLandscape = 0;
+        $this->FitPortrait = 1;
+        $this->HorizontalCenter = 0;
+        $this->ImageProtect = 1;
+        $this->KeyboardNav = 1;
+        $this->NewWindow = 1;
+        $this->PauseHover = 0;
+        $this->Performance = 1;
+        $this->Random = 0;
+        $this->Slideshow = 1;
+        $this->SlideInterval = 3000;
+        $this->SlideLinks = 0;
+        $this->StopLoop = 0;
+        $this->ThumbLinks = 0;
+        $this->ThumbnailNavigation = 0;
+        $this->Transition = 1;
+        $this->TransitionSpeed = 700;
+        $this->VerticalCenter = 1;
+    }
+
+    public function getCMSFields() {
+        $transitionItems = array(
+            0 => 'none',
+            1 => 'fade',
+            2 => 'slideTop',
+            3 => 'slideRight',
+            4 => 'slideBottom',
+            5 => 'slideLeft',
+            6 => 'carouselRight',
+            7 => 'carouselLeft'
+        );
+        
+        $performanceItems = array(
+            0 => 'No adjustments',
+            1 => 'Hybrid',
+            2 => 'Higher',
+            3 => 'Faster'
+        );
+        
+        $slideItems = array(
+            0 => 'Disable',
+            'num' => 'Numbers',
+            'name' => 'Title',
+            'blank' => 'Links'
+        );
+        $fields = parent::getCMSFields();
+        $fields->addFieldsToTab('Root.Content.Options',
+                array (
+                    new CheckboxField('AutoPlay','Auto Play'),
+                    new CheckboxField('FitAlways','Fit Always'),
+                    new CheckboxField('FitLandscape','Fit Landscape'),
+                    new CheckboxField('FitPortrait','Fit Portrait'),
+                    new CheckboxField('HorizontalCenter','Horizontal Center'),
+                    new CheckboxField('ImageProtect','Image Protect'),
+                    new CheckboxField('KeyboardNav','Keyboard Nav'),
+                    new NumericField('MinHeight','Min Height'),
+                    new NumericField('MinWidth','Min Width'),
+                    new CheckboxField('NewWindow','New window'),
+                    new CheckboxField('PauseHover','Pause Hover'),
+                    new DropdownField('Performance','performance',$performanceItems),
+                    new CheckboxField('Random','Random'),
+                    new CheckboxField('Slideshow','Slideshow'),
+                    new NumericField('SlideInterval','Slide Interval'),
+                    new DropdownField('SlideLinks','slide_links',$slideItems),
+                    new NumericField('StartSlide','Start Slide'),
+                    new CheckboxField('StopLoop','Stop Loop'),
+                    new CheckboxField('ThumbLinks','Thumb Links'),
+                    new CheckboxField('ThumbnailNavigation','Thumbnail Navigation'),
+                    new DropdownField('Transition','transition',$transitionItems),
+                    new NumericField('TransitionSpeed','Transition Speed'),
+                    new CheckboxField('VerticalCenter','Vertical Center')
+                )
+        );
+        return $fields;
+    }
+}
+
+class SuperSizedPage_Controller extends Page_Controller {
+    
+}
---
+++ templates/Layout/SuperSizedPage.ss
@@ -0,0 +1,121 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+	<!--
+		Supersized - Fullscreen Slideshow jQuery Plugin
+		Version : 3.2.7
+		Site	: www.buildinternet.com/project/supersized
+		
+		Author	: Sam Dunn
+		Company : One Mighty Roar (www.onemightyroar.com)
+		License : MIT License / GPL License
+                Silverstripe module version by : Luis E. S. Dias ( www.smartbyte.com.br )
+	-->
+
+	<head>
+
+		<title>Supersized - Full Screen Background Slideshow jQuery Plugin</title>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		<link rel="stylesheet" href="{$BaseHref}supersized/css/supersized.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="{$BaseHref}supersized/css/supersized.shutter.css" type="text/css" media="screen" />
+		
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<script type="text/javascript" src="{$BaseHref}supersized/javascript/jquery.easing.min.js"></script>
+		<script type="text/javascript" src="{$BaseHref}supersized/javascript/supersized.3.2.7.js"></script>
+		<script type="text/javascript" src="{$BaseHref}supersized/javascript/supersized.shutter.js"></script>
+		<script type="text/javascript">
+			
+			jQuery(function($){
+				
+				$.supersized({
+                                    // Functionality
+                                    autoplay : $AutoPlay,
+                                    fit_always : $FitAlways,
+                                    fit_landscape : $FitLandscape,
+                                    fit_portrait : $FitPortrait,
+                                    horizontal_center : $HorizontalCenter,
+                                    image_protect : $ImageProtect,
+                                    keyboard_nav : $KeyboardNav,
+                                    min_height : $MinHeight,
+                                    min_width : $MinWidth,
+                                    new_window : $NewWindow,
+                                    pause_hover : $PauseHover,
+                                    performance : $Performance,
+                                    random : $Random,
+                                    slideshow : $Slideshow,
+                                    slide_interval : $SlideInterval,
+                                    slide_links : '$SlideLinks',
+                                    start_slide : $StartSlide,
+                                    stop_loop : $StopLoop,
+                                    thumb_links : $ThumbLinks,
+                                    thumbnail_navigation : $ThumbnailNavigation,
+                                    transition : $Transition,
+                                    transition_speed : $TransitionSpeed,
+                                    vertical_center : $VerticalCenter,
+                                    slides : [			// Slideshow Images
+                                    <% control Children %>
+                                        <% if Last %>
+                                            {image : '$getBaseUrl$image.URL', title : '$title', thumb : '$getBaseUrl$thumb.URL'}
+                                        <% else %>
+                                            {image : '$getBaseUrl$image.URL', title : '$title', thumb : '$getBaseUrl$thumb.URL'},
+                                        <% end_if %>
+                                    <% end_control %>
+                                    ]
+                            });
+		    });
+		</script>
+		
+	</head>
+	
+	<style type="text/css">
+		ul#demo-block{ margin:0 15px 15px 15px; }
+			ul#demo-block li{ margin:0 0 10px 0; padding:10px; display:inline; float:left; clear:both; color:#aaa; background:url('{$BaseHref}supersized/images/bg-black.png'); font:11px Helvetica, Arial, sans-serif; }
+			ul#demo-block li a{ color:#eee; font-weight:bold; }
+	</style>
+
+<body>
+
+	<!--Thumbnail Navigation-->
+	<div id="prevthumb"></div>
+	<div id="nextthumb"></div>
+	
+	<!--Arrow Navigation-->
+	<a id="prevslide" class="load-item"></a>
+	<a id="nextslide" class="load-item"></a>
+	
+	<div id="thumb-tray" class="load-item">
+		<div id="thumb-back"></div>
+		<div id="thumb-forward"></div>
+	</div>
+	
+	<!--Time Bar-->
+	<div id="progress-back" class="load-item">
+		<div id="progress-bar"></div>
+	</div>
+	
+	<!--Control Bar-->
+	<div id="controls-wrapper" class="load-item">
+		<div id="controls">
+			
+			<a id="play-button"><img id="pauseplay" src="{$BaseHref}supersized/images/pause.png"/></a>
+		
+			<!--Slide counter-->
+			<div id="slidecounter">
+				<span class="slidenumber"></span> / <span class="totalslides"></span>
+			</div>
+			
+			<!--Slide captions displayed here-->
+			<div id="slidecaption"></div>
+			
+			<!--Thumb Tray button-->
+			<a id="tray-button"><img id="tray-arrow" src="{$BaseHref}supersized/images/button-tray-up.png"/></a>
+			
+			<!--Navigation-->
+			<ul id="slide-list"></ul>
+			
+		</div>
+	</div>
+
+</body>
+</html>
